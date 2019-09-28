79eee2c7d375760afa0c2b964cd20f75e124d74b
catonrug <aigars.kadikis@gmail.com>
2019-09-28 12:04:00 -0400

    next

--- reconfigure.sh
+++ reconfigure.sh
@@ -6,8 +6,6 @@ sudo timedatectl set-timezone Europe/Riga
 sudo apt -y update
 sudo apt -y upgrade
 
-# install docker containers
-
 # install rclone
 curl https://rclone.org/install.sh | sudo bash
 
@@ -35,22 +33,26 @@ mkdir -p ~/Downloads
 curl -L https://github.com/catonrug/xbmc-lattelecom.tv/archive/master.zip > ~/Downloads/xbmc-lattelecom.tv.zip
 cd
 
+# nice video player
+sudo apt -y install mpv
+sudo apt -y install youtube-dl
+
+
 # pip module
-sudo apt -y install python-pip
+
+# sudo apt -y install python-pip
 
 # youtuve download utility
-sudo pip install --upgrade youtube-dl
+# sudo pip install --upgrade youtube-dl
 # to merge audio together with video
 sudo apt -y install ffmpeg
 
 # install youtube channel management through directory structure
-curl -s "https://catonrug.blogspot.com/feeds/posts/default/5255522565948307079?alt=json" | jq -r '.entry|.content|."$t"' | base64 --decode | sudo tee /usr/bin/renew-youtube-channel
-sudo chmod +x /usr/bin/renew-youtube-channel
-curl -s "https://catonrug.blogspot.com/feeds/posts/default/905125781365764435?alt=json" | jq -r '.entry|.content|."$t"' | base64 --decode | sudo tee /usr/bin/renew-all-channels
-sudo chmod +x /usr/bin/renew-all-channels
+# curl -s "https://catonrug.blogspot.com/feeds/posts/default/5255522565948307079?alt=json" | jq -r '.entry|.content|."$t"' | base64 --decode | sudo tee /usr/bin/renew-youtube-channel
+# sudo chmod +x /usr/bin/renew-youtube-channel
+# curl -s "https://catonrug.blogspot.com/feeds/posts/default/905125781365764435?alt=json" | jq -r '.entry|.content|."$t"' | base64 --decode | sudo tee /usr/bin/renew-all-channels
+# sudo chmod +x /usr/bin/renew-all-channels
 
-# remove built in torrent client
-sudo apt -y remove transmission-gtk
 
 # https://mxlinux.org/wiki/applications/docker/
 wget -O docker.gpg  https://download.docker.com/linux/debian/gpg 
@@ -60,6 +62,9 @@ CODENAME=stretch
 echo "deb [arch=amd64] https://download.docker.com/linux/debian $CODENAME  stable" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt -y update
 sudo apt -y install docker-ce
+
+# remove built in torrent client
+sudo apt -y remove transmission-gtk vlc
 
 # remove obsolete packages
 sudo apt -y autoremove
